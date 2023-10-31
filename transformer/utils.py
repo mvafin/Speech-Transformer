071eebb7549b66bae2cb93e3391fe99749389456
Maxim Vafin <maxim.vafin@intel.com>
2023-10-31 12:01:50 +0100

    Update utils.py

--- transformer/utils.py
+++ transformer/utils.py
@@ -8,7 +8,7 @@ def pad_list(xs:List[torch.Tensor], pad_value:int):
     max_len = max(x.size(0) for x in xs)
     s = [n_batch, max_len]
     #s.extend(xs[0].size()[1:])
-    pad = xs[0].new(n_batch, max_len, *xs[0].size()[1:]).fill_(pad_value)
+    pad = xs[0].new_full(s, pad_value)
     for i in range(n_batch):
         pad[i, :xs[i].size(0)] = xs[i]
     return pad
